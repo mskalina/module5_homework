1cab292d66315004e5b42cee04359c18865cf8c7
mskalina <89806037+mskalina@users.noreply.github.com>
2021-09-27 01:06:56 +0300

    Create ex8.js

---
+++ ex8.js
@@ -0,0 +1,8 @@
+let wish = new Map([
+    ["Nik", "SMK"],
+    ["City", "SpB"],
+    ["Wish",    "Be happy"]
+]);
+wish.forEach((value, key) => {
+    console.log(`Ключ - ${key}, значение - ${value}`);
+});
